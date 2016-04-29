6525c946c8016f63e8e0114b894ddd60bee83269
jaimeh760 <jaimeh760@gmail.com>
2016-04-29 15:08:21 +0000

    Finished Fwitter 5!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,27 +36,27 @@ class ApplicationController < Sinatra::Base
   end
   
   get '/signup' do
-    "Hello World"
+    erb :signup
   end
 
   post '/sign-up' do 
-    @user = User.new(:username => params[:username], :email => params[:email])
+    @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
     @user.save
-    redirect '/'
+    redirect '/login'
   end
   
-  get 'login' do
-    erb:login
+  get '/login' do
+    erb :login
   end
   
   post '/login' do
-    user = User.find_by(:username => params[:username])
-    if user
-      session[:user_id] = user_id
-      redirect "/"
-    else
-      redirect "/signup"
-    end
+     user = User.find_by(:username => params[:username])
+     if user
+        session[:user_id] = user.id
+        redirect "/"
+     else
+        redirect "/signup"
+     end
   end
   
   get '/logout' do
